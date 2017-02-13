ea62911340216167110e944ef88f7381168b349e
aiyanhe <1152423015@qq.com>
2017-02-13 11:07:23 +0800

    git@git.oschina.net:aiyanhe/aiyanhe.git

---
+++ 面向对象编程——类/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 面向对象编程__类
+{
+    class Car
+    {
+       public float speed;
+        public float value;
+        public string carName;
+
+        public void Move()
+        {
+            Console.WriteLine("我移动啦");
+
+        }
+
+    }
+}
---
+++ 面向对象编程——类/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 面向对象编程__类
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car bus = new Car();
+            bus.carName = "广龙日天大帝";
+            bus.speed = 1000;
+            bus.value = 10000000;
+
+            bus.Move();
+            Console.ReadKey();
+        }
+    }
+}
