7499f8da662f0db9376157844356fecb9c22cc8d
aiyanhe <1152423015@qq.com>
2017-02-14 09:58:09 +0800

    20170214

---
+++ 013-密封类和密封方法/dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013_密封类和密封方法
+{
+    //class dog:Animal  无法从密封的类中继承
+    class dog:Cat
+    {
+        public sealed override void Move()//我们可以把重写的方法声明为密封方法，表示该方法不能被再次重写；
+        {
+            Console.WriteLine("这里是dog的Move");
+        }
+    }
+}
