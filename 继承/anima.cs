f43521e6b7ef6959c445711947f52e3376b07c43
aiyanhe <1152423015@qq.com>
2017-02-14 18:10:33 +0800

    20170214

---
+++ 继承/anima.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 继承
+{
+    class anima
+    {
+        public string name;
+        public int age;
+        private string cost;
+        protected string color;
+        //public 公共的，无访问限制
+        //private 私有的，只有本类可以访问
+        //protected 受保护的，只有本类和子类可以访问到（要注意区别）
+        public anima(string name)
+        {
+           // Console.WriteLine("anima的有参构造函数");
+
+        }
+        public anima()
+        {
+
+        }
+        public virtual void Run()//关键字 virtual 将该方法变为虚方法，方便子类重写；
+        {
+            Console.WriteLine("anima在跑");
+
+        }
+        public void Say()
+        {
+            Console.WriteLine("anima在叫");
+        }
+    }
+}
---
+++ 继承/dog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 继承
+{
+    class dog:anima
+    {
+        //string name1 = "金毛";
+        
+        public dog():base("sds")//表示调用父类的相对应参数的构造函数
+        {
+            //Console.WriteLine("这里是dog的构造函数");
+
+        }
+        //public new void Run()//加了关键字new，让编译器知道我们隐藏父类的run方法,自建一个新的run方法，当然，new也可以不用加;
+        //{
+        //    Console.WriteLine("这里是dog的run");
+        //}
+        public override void Run()//关键字override ,重写父类相对应的虚方法
+        {
+            //base.Run(); //base 基于父类 this 基于本类
+            Console.WriteLine("这里是dog的run");
+        }
+        public new void Say()
+        {
+            Console.WriteLine("dog在叫");
+        }
+        public void jia()
+        {
+           // cost = 100;//两个要在方法下测试！！
+            color = "red";
+        }
+        
+
+    }
+}
