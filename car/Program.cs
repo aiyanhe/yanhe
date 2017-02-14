e9431ea5024740da8e46cd0445bcc7ad71683bbe
aiyanhe <1152423015@qq.com>
2017-02-14 11:29:35 +0800

    20170214

---
+++ car/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bus car = new smallbus();
+            car.Move();
+            smallbus car1 = new smallbus();
+            car1.Move();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ car/bus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car
+{
+    class bus
+    {
+        public void Move()
+        {
+            Console.WriteLine("这里是bus的move");
+
+        }
+    }
+}
