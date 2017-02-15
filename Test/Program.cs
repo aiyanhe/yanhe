a5de5a8af072957847820653a26cb775a0903769
aiyanhe <1152423015@qq.com>
2017-02-15 17:49:39 +0800

    20170215

---
+++ Test/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Father
+    {
+        public void F()
+        {
+            Console.WriteLine("A.F");
+        }
+        public virtual void G()
+        {
+            Console.WriteLine("A.G");
+        }
+    }
+    class Son : Father
+    {
+        new public void F()//有new的情况下这个F()方法是子类独有的；
+        {
+            Console.WriteLine("B.F");
+        }
+        public override void G()
+        {
+            Console.WriteLine("B.G");
+        }
+    }
+    class override_new
+    {
+        static void Main()
+        {
+            Son b = new Son();
+            Father a = b;
+            a.F();//AF
+            b.F();//BF
+            a.G();//BG
+            b.G();//BG
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ TroubleTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TroubleTest
+{
+    public class A
+    {
+        public virtual void Fun1(int i)
+        {
+            Console.WriteLine(i);
+        }
+        public void Fun2(A a)
+        {
+            a.Fun1(1);
+            Fun1(5);
+        }
+    }
+    public class B : A
+    {
+        public override void Fun1(int i)
+        {
+            base.Fun1(i + 1);
+        }
+        public static void Main()
+        {
+            B b = new B();
+            A a = new A();
+            a.Fun2(b); 
+
+            b.Fun2(a); 
+
+        }
+    }
+}
