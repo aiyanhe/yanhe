ae2437b51a54aaee1a2e990b4a073fa5db828d10
aiyanhe <1152423015@qq.com>
2017-02-21 22:05:24 +0800

    20170221

---
+++ TestMaoPao/Class.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaoPao
+{
+    class Class
+    {
+        private string name{get;set;}
+        private float sore { get; set; }
+
+        public Class(string name ,float sore)
+        {
+            this.name = name;
+            this.sore = sore;
+        }
+        public static bool GetClass(Class c1,Class c2)
+        {
+            if (c1.sore>c2.sore)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public string OO()
+        {
+            return name + ":" + sore;
+        }
+    }
+}
