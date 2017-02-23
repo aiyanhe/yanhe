6d92e7b19f6570b9a88a322136e422f355bd598d
aiyanhe <1152423015@qq.com>
2017-02-23 09:00:21 +0800

    20170222

---
+++ 007-事件/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_事件
+{
+    class Program
+    {
+        public delegate void yanhe();//
+        //public yanhe tianyi;
+        public event yanhe tianyi;
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.tianyi = Test;
+            p.tianyi();
+            Console.ReadKey();
+        }
+        public static void Test()
+        {
+            Console.WriteLine("哎，言和");
+        }
+    }
+}
