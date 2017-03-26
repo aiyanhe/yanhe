85dc0ef94679b5c8f42575bc89321d929b33857b
aiyanhe <1152423015@qq.com>
2017-03-26 23:21:02 +0800

    2021

--- 020——线程_任务/Program.cs
+++ 020——线程_任务/Program.cs
@@ -11,17 +11,40 @@ namespace _020__线程_任务
     {
         public static void Text()
         {
-            Console.WriteLine("开始下载" + Thread.CurrentThread.ManagedThreadId);//当前线程的Id
+            Console.WriteLine("t1父级任务开始下载" + Thread.CurrentThread.ManagedThreadId);//当前线程的Id
+           
             Thread.Sleep(2000);
-            Console.WriteLine("下载完成");
+            Task t3 = new Task(Text3);
+            t3.Start();
+            Console.WriteLine("t1父级任务完成");
+        }
+        public static void Text2(Task k)
+        {
+            Console.WriteLine("Text2开始下载" + Thread.CurrentThread.ManagedThreadId);//当前线程的Id
+            Thread.Sleep(2000);
+            Console.WriteLine("Text2下载完成");
+        }
+        public static void Text3()
+        {
+            Console.WriteLine("子级任务开始");
+            Thread.Sleep(5000);
+            Console.WriteLine("子级任务完成");
         }
         static void Main(string[] args)
         {
             Task t = new Task(Text);//传递一个需要线程去执行的方法
             t.Start();
-
+            Console.WriteLine("父级任务未完成时状态" + t.Status);
+            Thread.Sleep(2000);
+            Console.WriteLine("父级任务完成时状态" + t.Status);
+            Thread.Sleep(8000);
+            Console.WriteLine("父级任务完成时状态" + t.Status);
+            //连续任务ContinueWith
+            //Task t1 = t.ContinueWith(Text2);//当t任务完成后才执行t1任务
+            //t1.Start();//连续任务的下级任务不用Start();
             TaskFactory tf = new TaskFactory();//开启一个任务工厂
-            Task t2 = tf.StartNew(Text);
+            //Task t2 = tf.StartNew(Text);
+            Console.ReadKey();
         }
     }
 }
