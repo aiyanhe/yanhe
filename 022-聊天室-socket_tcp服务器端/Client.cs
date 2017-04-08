f4298e948dece62beb9a295103f44caea35fad03
aiyanhe <1152423015@qq.com>
2017-04-08 19:52:31 +0800

    2017

---
+++ 022-聊天室-socket_tcp服务器端/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _022_聊天室_socket_tcp服务器端
+{
+    class Client
+    {
+        public Client(Socket s)
+        {
+
+        }
+    }
+}
